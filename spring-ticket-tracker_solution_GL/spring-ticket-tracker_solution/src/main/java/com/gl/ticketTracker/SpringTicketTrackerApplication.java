9b0e7e88c7856ad0f7ecf7365182909001773344
Sanskriti007 <110283840+Sanskriti007@users.noreply.github.com>
2023-10-02 22:54:20 +0530

    Add files via upload

---
+++ spring-ticket-tracker_solution_GL/spring-ticket-tracker_solution/src/main/java/com/gl/ticketTracker/SpringTicketTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.gl.ticketTracker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTicketTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTicketTrackerApplication.class, args);
+	}
+
+}
